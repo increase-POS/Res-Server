c88d487825f981d1a69145accdfc948a2140c3f2
ahmadms2020 <najyms@gmail.com>
2021-12-14 16:57:25 +0200

    a

--- serverfolder/POS_Server/Models/AgentMembershipsModel.cs
+++ serverfolder/POS_Server/Models/AgentMembershipsModel.cs
@@ -7,18 +7,25 @@ namespace POS_Server.Models
 {
     public class AgentMembershipsModel
     {
+
+        public int agentMembershipsId { get; set; }
+        public Nullable<int> subscriptionFeesId { get; set; }
+        public Nullable<int> cashTransId { get; set; }
         public Nullable<int> membershipId { get; set; }
         public Nullable<int> agentId { get; set; }
         public Nullable<System.DateTime> startDate { get; set; }
         public Nullable<System.DateTime> EndDate { get; set; }
-        public Nullable<decimal> Amount { get; set; }
         public string notes { get; set; }
         public Nullable<System.DateTime> createDate { get; set; }
         public Nullable<System.DateTime> updateDate { get; set; }
         public Nullable<int> createUserId { get; set; }
         public Nullable<int> updateUserId { get; set; }
-        public Nullable<byte> isActive { get; set; }
-        public int agentMembershipsId { get; set; }
+        public byte isActive { get; set; }
+
+
+     
+        public decimal Amount { get; set; }
+      
 
         public bool canDelete { get; set; }
     }
