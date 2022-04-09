3a92f1c041deed186aec0278c2c06d5410f2b1e4
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2022-04-09 10:54:13 +0200

    GetOrdersWithDelivery

--- serverfolder/POS_Server/Controllers/OrderPreparingController.cs
+++ serverfolder/POS_Server/Controllers/OrderPreparingController.cs
@@ -460,7 +460,7 @@ namespace POS_Server.Controllers
                             foreach (OrderPreparingModel o in prepOrders)
                             {
                                 #region set inv status
-                                if (o.status != "Ready")
+                                if (o.status == "Listed" || o.status == "preparing")
                                 {
                                     inv.status = "Listed";
                                     break;
@@ -473,6 +473,119 @@ namespace POS_Server.Controllers
                         }
 
                         
+                        return TokenManager.GenerateToken(invoices);
+                    }
+                }
+                catch
+                {
+                    return TokenManager.GenerateToken("0");
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("GetOrdersWithDelivery")]
+        public string GetOrdersWithDelivery(string token)
+        {
+            token = TokenManager.readToken(HttpContext.Current.Request);
+            var strP = TokenManager.GetPrincipal(token);
+            if (strP != "0") //invalid authorization
+            {
+                return TokenManager.GenerateToken(strP);
+            }
+            else
+            {
+                #region params
+                string statusStr = "";
+                List<string> statusL = new List<string>();
+                int branchId = 0;
+                IEnumerable<Claim> claims = TokenManager.getTokenClaims(token);
+                foreach (Claim c in claims)
+                {
+                    if (c.Type == "branchId")
+                    {
+                        branchId =int.Parse(c.Value);
+                    }
+                    else if (c.Type == "status")
+                    {
+                        statusStr = c.Value;
+                        string[] statusArray = statusStr.Split(',');
+                        foreach (string s in statusArray)
+                            statusL.Add(s.Trim());
+                    }
+                }
+                #endregion
+                try
+                {
+                    using (incposdbEntities entity = new incposdbEntities())
+                    {
+                        var searchPredicate = PredicateBuilder.New<invoices>();
+                        searchPredicate = searchPredicate.And(x => x.branchId == branchId);
+
+                        searchPredicate = searchPredicate.And(x => x.invType =="ts" || x.invType == "ss");
+                        searchPredicate = searchPredicate.And(x => x.shippingCompanyId != null);
+                       
+
+                        var invoices = entity.invoices.Where(searchPredicate)
+                                                    .Select(x => new InvoiceModel() {
+                                                    invNumber = x.invNumber,                                                  
+                                                    invoiceId= x.invoiceId}).ToList();
+
+
+                        foreach(InvoiceModel inv in invoices)
+                        {
+                            var prepOrders = (from o in entity.orderPreparing.Where(x=> x.invoiceId == inv.invoiceId)
+                                              join s in entity.orderPreparingStatus on o.orderPreparingId equals s.orderPreparingId
+                                              where (s.orderStatusId == entity.orderPreparingStatus.Where(x => x.orderPreparingId == o.orderPreparingId).Max(x => x.orderStatusId))
+                                              select new OrderPreparingModel()
+                                              {
+                                                  orderPreparingId = o.orderPreparingId,
+                                                  invoiceId = o.invoiceId,
+                                                  notes = o.notes,
+                                                  orderNum = o.orderNum,
+                                                  createDate = o.createDate,
+                                                  createUserId = o.createUserId,
+                                                  invNum = o.invoices.invNumber,
+                                                  status = s.status,
+                                              }).ToList();
+
+
+                            foreach (OrderPreparingModel o in prepOrders)
+                            {
+                                #region set inv status
+                                if (o.status == "Collected")
+                                {
+                                    inv.status = "Collected";
+                                    break;
+                                }
+                                else if (o.status == "InTheWay")
+                                {
+                                    inv.status = "InTheWay";
+                                    break;
+                                }
+                                else if (o.status == "Done")
+                                {
+                                    inv.status = "Done";
+                                    break;
+                                }
+                                else if (o.status == "Listed" || o.status == "Preparing")
+                                {
+                                    inv.status = "Listed";
+                                    break;
+                                }
+                                else
+                                    inv.status = "Ready";
+                                #endregion
+
+                            }
+
+                        }
+
+                        #region get orders according to status
+                        if (statusStr != "")
+                            invoices = invoices.Where(x => statusL.Contains(x.status)).OrderBy(x => x.invNumber).ToList();
+                        #endregion
+
                         return TokenManager.GenerateToken(invoices);
                     }
                 }
